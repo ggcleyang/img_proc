a84f3594c30fcf598e0512ffc624bc1d5e5cd670
ggcleyang <20198123+ggcleyang@users.noreply.github.com>
2022-05-16 23:16:59 +0800

    Add files via upload

--- img_pro.c
+++ img_pro.c
@@ -1,5 +1,6 @@
 
 #include <math.h>
+//#include <tgmath.h.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include "timing.h"
@@ -42,7 +43,6 @@
 #define int64   long long
 #define uint64  unsigned long long
 #define CLIP3(x,min,max)         ( (x)< (min) ? (min) : ((x)>(max)?(max):(x)) )
-char *out_img = "gtm_gamma_img.bmp";
 
 #define ASYMMETRY_LUT_SIZE 65
 #define ISP_DRC_DATA_MAX    ((1<<8) - 1)
@@ -200,25 +200,229 @@ uint8 linear_inter(uint8 x0,uint8 y0,uint8 x1,uint8 y1,float x){
     uint8 result = CLIP3(y0 + k*(x-x0),0,255);
      return result;
 }
+//generate gauss filter kernel
+double* make_gauss_kernel(int size, double sigma) {
+    if (size % 2 == 0) {
+        printf("size must be odd");
+        return NULL;
+    }
+    double *kernel = (double *)malloc(size*size*sizeof(double ));
+    double d_sigma_2 = 2 * sigma * sigma;
+    double c = 1 / (3.141592654 * d_sigma_2);
+    double sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            double x = i - floor(size / 2);
+            double y = k - floor(size / 2);
+
+            kernel[i*size+k] = exp(-(x * x + y * y) / d_sigma_2) * c;
+            sum += kernel[i*size + k];
+        }
+    }
+
+    for (int i = 0; i < size*size; i++) {
+        kernel[i] = kernel[i] / sum;
+    }
+    return kernel;
+}
+//bilateral filtering
+int   kernel_size = 3;
+double  sigma_s = 100;//space domain sigma
+double  sigma_r = 100;//range domain sigma
+void bilateral_filter(uint8* input,uint8* output,uint16 img_width,uint16 img_height){
+
+    memcpy(output, input, img_width * img_height*sizeof(uint8));
+    double *gauss_kernel = make_gauss_kernel(kernel_size,sigma_s);
+    uint8 half_size = kernel_size/2;
+    for (uint16 y = half_size; y < img_height-half_size; y++) {
+        for (uint16 x = half_size; x < img_width-half_size; x++) {
+            uint8 center_Y = input[y*img_width+x];
+            double sum_w = 0.0;
+            double out_Y = 0.0;
+
+            for (int8 ky = -half_size; ky <= half_size; ky++) {
+                for (int8 kx = -half_size; kx <= half_size; kx++) {
+
+                    uint8 temp_Y = input[(y+ky)*img_width+x+kx];
+                    double w1 = gauss_kernel[(ky+half_size)*kernel_size+kx+half_size];
+
+                    uint8 d_Y = abs(temp_Y-center_Y);
+                    double w2 = exp(-0.5*(d_Y*d_Y)/(sigma_r*sigma_r));
+                    //w2 = 1.0;
+                    //w1 = 1.0;
+                    double w = w1*w2;
+
+                    out_Y += temp_Y*w;
+                    sum_w +=w;
+                }
+            }
+            out_Y = out_Y/sum_w;
+            output[y*img_width+x] = CLIP3(round(out_Y),0,255);
+        }
+    }
+    free(gauss_kernel);
+    return;
+}
+#if 0
+void bilateral_filter(uint8* input,uint8* output,uint16 img_width,uint16 img_height){
+
+    memcpy(output, input, img_width * 3 * img_height*sizeof(uint8));
+    double *gauss_kernel = make_gauss_kernel(kernel_size,sigma_s);
+    uint8 half_size = kernel_size/2;
+    for (uint16 y = half_size; y < img_height-half_size; y++) {
+        for (uint16 x = half_size; x < img_width-half_size; x++) {
+            uint8 center_R = input[(y*img_width+x)*3];
+            uint8 center_G = input[(y*img_width+x)*3+1];
+            uint8 center_B = input[(y*img_width+x)*3+2];
+            double sum_w = 0.0;
+            double out_R = 0.0;
+            double out_G = 0.0;
+            double out_B = 0.0;
+            for (int8 ky = -half_size; ky <= half_size; ky++) {
+                for (int8 kx = -half_size; kx <= half_size; kx++) {
+
+                    uint8 temp_R = input[((y+ky)*img_width+x+kx)*3];
+                    uint8 temp_G = input[((y+ky)*img_width+x+kx)*3+1];
+                    uint8 temp_B = input[((y+ky)*img_width+x+kx)*3+2];
+
+                    double w1 = gauss_kernel[(ky+half_size)*kernel_size+kx+half_size];
+
+                    uint8 d_R = abs(temp_R-center_R);
+                    uint8 d_G = abs(temp_G-center_G);
+                    uint8 d_B = abs(temp_B-center_B);
+                    double w2 = exp(-0.5*(d_R*d_R+d_G*d_G+d_B*d_B)/(sigma_r*sigma_r));
+                    //w2 = 1.0;
+                    //w1 = 1.0;
+                    double w = w1*w2;
+
+                    out_R += temp_R*w;
+                    out_G += temp_G*w;
+                    out_B += temp_B*w;
+                    sum_w +=w;
+                }
+            }
+            out_R = out_R/sum_w;
+            out_G = out_G/sum_w;
+            out_B = out_B/sum_w;
+            output[(y*img_width+x)*3] = CLIP3(round(out_R),0,255);
+            output[(y*img_width+x)*3+1] = CLIP3(round(out_G),0,255);
+            output[(y*img_width+x)*3+2] = CLIP3(round(out_B),0,255);
+        }
+    }
+    free(gauss_kernel);
+    return;
+}
+#endif
+void operate_log_10(uint8* input,double* output,uint16 img_width,uint16 img_height){
+
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+            output[y*img_width+x] = CLIP3(log10(input[y*img_width+x]),0.0,3);
+        }
+    }
+
+    return;
+}
+void operate_pow_10(double* input,uint8* output,uint16 img_width,uint16 img_height){
+
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+            output[y*img_width+x] = round(pow(10,input[y*img_width+x]));
+        }
+    }
+
+    return;
+}
+uint8*get_min_max(uint8* input,uint16 img_width,uint16 img_height){
+
+    uint8* min_max = (uint8*) malloc(2*sizeof(uint8));
+    min_max[0] = input[0];
+    min_max[1] = input[0];
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+            uint8 temp_value = input[y*img_width+x];
+            if(temp_value <=min_max[0]){
+                min_max[0] = temp_value;
+            }
+            if(temp_value>=min_max[1]){
+                min_max[1] = temp_value;
+            }
+        }
+    }
+
+    return min_max;
+}
+///////////////////
 void simple_gtm(uint8* input,uint8* output,uint16 img_width,uint16 img_height,uint8 *lut){
 
     uint8 *intensity_in = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
-    uint8 *intensity_out = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
-
+    //uint8 *intensity_out = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
+    //double *log_intensity = (double *) malloc(img_width * img_height * sizeof(double));
+    //double *base_intensity = (double *) malloc(img_width * img_height * sizeof(double));
+    //double *detail_intensity = (double *) malloc(img_width * img_height * sizeof(double));
+    uint8 *base_intensity = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
+    uint8 *base_inter = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
+    uint8 *detail_intensity = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
+    uint8 *blend_intensity = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
+    //uint8 *test_out = (uint8 *) malloc(img_width * img_height * sizeof(uint8));
     //rgb to intensity,get intensity img
     //char *gray_in = "gray_in.bmp";
     //char *gray_out = "gray_out.bmp";
     rgb2gray(input,intensity_in,img_width,img_height);
+
+    char *test_imgxx = "test_original.bmp";
+    single2BMP(intensity_in,img_width,img_height,test_imgxx);
+
+    bilateral_filter(intensity_in,base_intensity,img_width,img_height);
+    char *test_base_out = "test_base.bmp";
+    single2BMP(base_intensity,img_width,img_height,test_base_out);
+
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+            detail_intensity[y*img_width+x] = abs(intensity_in[y*img_width+x] - base_intensity[y*img_width+x]);
+        }
+    }
+    char *test_detail_out = "test_detail.bmp";
+    single2BMP(detail_intensity,img_width,img_height,test_detail_out);
+#if 0
+    uint8 *pixel_min_max;
+    pixel_min_max = get_min_max(base_intensity,img_width,img_height);
+    //printf("pixel_min_max:min-%d,max-%d",pixel_min_max[0],pixel_min_max[1]);
+    uint8 contrast = 128;
+    double compress_factor = (double)contrast/(pixel_min_max[1]-pixel_min_max[0]);
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+            uint8 temp_pixel = CLIP3(round(0.9*base_intensity[y*img_width+x] + detail_intensity[y*img_width+x]),0,255);
+            blend_intensity[y*img_width+x] = temp_pixel;
+        }
+    }
+
+#endif
+#if 1
     //single2BMP(intensity_in,img_width,img_height,gray_in);
     for(uint16 y = 0;y < img_height;y++){
         for(uint16 x =0;x < img_width;x++){
             uint8 temp_Y = intensity_in[y*img_width+x];
             uint8 index_Y = temp_Y/4;
             float float_Y = (float)temp_Y/4;
-            intensity_out[y*img_width+x] = linear_inter(index_Y,lut[index_Y],index_Y+1,lut[index_Y+1],float_Y);
-            //intensity_out[y*img_width+x] = gamma_045_table[temp_Y];
+            //base_inter[y*img_width+x] = linear_inter(index_Y,lut[index_Y],index_Y+1,lut[index_Y+1],float_Y);
+            base_inter[y*img_width+x] = gamma_045_table[temp_Y];
         }
     }
+    for(uint16 y = 0;y < img_height;y++){
+        for(uint16 x =0;x < img_width;x++){
+
+            uint16 temp_value = base_inter[y*img_width+x] - 0.3*detail_intensity[y*img_width+x];
+            //uint16 temp_value = base_inter[y*img_width+x];
+
+            blend_intensity[y*img_width+x] = CLIP3(temp_value,0,255);
+        }
+    }
+    //char *blend_out = "test_blend.bmp";
+    //single2BMP(blend_intensity,img_width,img_height,blend_out);
+#endif
+#if 1
     //single2BMP(intensity_out,img_width,img_height,gray_out);
     // rgb * scale factor
     for(uint16 y = 0;y < img_height;y++){
@@ -229,7 +433,7 @@ void simple_gtm(uint8* input,uint8* output,uint16 img_width,uint16 img_height,ui
             }
             else
             {
-                scale =  (float)intensity_out[y*img_width+x]/intensity_in[y*img_width+x];
+                scale =  (float)blend_intensity[y*img_width+x]/intensity_in[y*img_width+x];
             }
 
             //printf("scale:%f\n",scale);
@@ -242,11 +446,14 @@ void simple_gtm(uint8* input,uint8* output,uint16 img_width,uint16 img_height,ui
             output[3*(y*img_width+x)+2] =CLIP3(temp_B,0,255);
         }
     }
-
-
+#endif
+    free(detail_intensity);
+    free(blend_intensity);
+    free(base_intensity);
+    //free(log_intensity);
     free(intensity_in);
-    free(intensity_out);
-
+    //free(intensity_out);
+    //free(pixel_min_max);
     return;
 }
 int main(int argc, char **argv) {
@@ -279,7 +486,8 @@ int main(int argc, char **argv) {
     inputImage = loadImage(szfile, &Width, &Height, &Channels);
     double nLoadTime = calcElapsed(startTime, now());
     printf("load time: %d ms.\n ", (int) (nLoadTime * 1000));
-
+    //char *Image = "input_test.bmp";
+    //save2BMP(inputImage,Width,Height,Image);
     if ((Channels != 0) && (Width != 0) && (Height != 0)) {
         uint8 *outputImg = (uint8 *) malloc(Width * Channels * Height * sizeof(uint8));
         if (inputImage) {
@@ -294,8 +502,13 @@ int main(int argc, char **argv) {
     //    apply_invert_gamma(outputImg,linear_img, Width,Height);
         GenerateAsymmetry(ispDrcS);
         simple_gtm(outputImg,gtm_img,Width,Height,ispDrcS->au8AsymmetryLUT);
+        char *out_img = "result_bf.bmp";
         save2BMP(gtm_img,Width,Height,out_img);
-
+        //double startTime1 = now();
+        //bilateral_filter(outputImg,gtm_img,Width,Height);
+        //double finish_time = calcElapsed(startTime1, now());
+        //save2BMP(gtm_img,Width,Height,out_img);
+        //printf("finish_time: %d ms.\n ", (int) (finish_time * 1000));
         free(gtm_img);
         free(outputImg);
         free(inputImage);
